d48ed27a3851bdffcd575b3ea597d13cf2222b42
Rodrigo Matheus De Oliveira <143840626+ASTECA2@users.noreply.github.com>
2025-08-21 22:59:22 +0000

    Corrige configuração do Vite para forçar caminhos relativos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Site---VORTEX/',
+  base: '', // Deixe a base vazia para forçar caminhos relativos
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
